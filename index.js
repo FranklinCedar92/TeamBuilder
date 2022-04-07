a15345f73b98b9fc8253c08450b01eb7e398ae11
Franklin <franklin.c.brown@gmail.com>
2022-04-06 17:07:48 -0700

    added page-template.js and card files

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const addEmployee =() => {
                 default: false
             }]).then(function (response){
                 if (response.confirmAddEmployee) {
-                    questions()
+                    questions() //even if Yes, this is writing the file
                 }
                 else {
                     var HTML = `<!doctype html>
@@ -31,10 +31,10 @@ const addEmployee =() => {
                         <!-- Bootstrap CSS -->
                         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
                     
-                        <title>Hello, world!</title>
+                        <title>Team Builder</title>
                       </head>
                       <body>
-                        <h1>Hello, world!</h1>
+                        <h1>Team Builder</h1>
                         ${managerHTML}
                         ${engineerHTML}
                         ${internHTML}
@@ -112,13 +112,13 @@ const addManager = (name, id, email,role) => {
             managerHTML += `<div class="card">
              <div class="card-header">
                ${newManager.name}
-               <h3>Manger</h3>
-               <h4>${newManager.officeNumber}</h4>
+               <h3>Manager</h3>
+               <h4>Office #${newManager.officeNumber}</h4>
              </div>
              <div class="card-body">
                <blockquote class="blockquote mb-0">
-                 <p>${newManager.id}.</p>
-                 <footer class="blockquote-footer"><a href="mailto://${newManager.email}">${newManager.email}</a></footer>
+                 <p>Employee ID: ${newManager.id}.</p>
+                 <footer class="blockquote-footer">email: <a href="mailto://${newManager.email}">${newManager.email}</a></footer>
                </blockquote>
              </div>
            </div>`
@@ -140,12 +140,12 @@ const addEngineer = (name, id, email, role) => {
              <div class="card-header">
                ${newEngineer.name}
                <h3>Engineer</h3>
-               <h4>${newEngineer.github}</h4>
+               <h4>GitHub: ${newEngineer.github}</h4>
              </div>
              <div class="card-body">
                <blockquote class="blockquote mb-0">
-                 <p>${newEngineer.id}.</p>
-                 <footer class="blockquote-footer"><a href="mailto://${newEngineer.email}">${newEngineer.email}</a></footer>
+                 <p>Employee ID: ${newEngineer.id}.</p>
+                 <footer class="blockquote-footer">email: <a href="mailto://${newEngineer.email}">${newEngineer.email}</a></footer>
                </blockquote>
              </div>
            </div>`
@@ -167,12 +167,12 @@ const addIntern = (name, id, email, role) => {
              <div class="card-header">
                ${newIntern.name}
                <h3>Intern</h3>
-               <h4>${newIntern.school}</h4>
+               <h4>School: ${newIntern.school}</h4>
              </div>
              <div class="card-body">
                <blockquote class="blockquote mb-0">
-                 <p>${newIntern.id}.</p>
-                 <footer class="blockquote-footer"><a href="mailto://${newIntern.email}">${newIntern.email}</a></footer>
+                 <p>Employee ID: ${newIntern.id}.</p>
+                 <footer class="blockquote-footer">email: <a href="mailto://${newIntern.email}">${newIntern.email}</a></footer>
                </blockquote>
              </div>
            </div>`
---
+++ src/page-template.js
@@ -0,0 +1,27 @@
+var HTML = `<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+        <title>Team Builder</title>
+    </head>
+    <body>
+        <div>
+            <header>
+                <h1 class="m_header m_container-fluid m_center m_bg-secondary>Team Organizer</h1>
+            </header>
+        </div>
+        <!-- Employee Cards -->
+        <div>
+        ${managerCard}
+        ${engineerCard}
+        ${internCard}
+        </div>
+    </body>
+</html>
+`
